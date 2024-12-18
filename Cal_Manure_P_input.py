Revised PN ratio calculation. Upscale method is wrong, need to be updated

Cal_Manure_P_input.py
@@ -1,4 +1,5 @@
 # This code is used to: Calculate P manure input based on N manure input and PN ratio
+# P_manure input would be added to N_Manure_Input.nc files after running this code
 
 import xarray as xr
 import netCDF4 as nc
@@ -17,10 +18,9 @@ PNratio_broadcasted = np.expand_dims(PNratio_transposed, axis=0)  # Add time dim
 PNratio_broadcasted = np.repeat(PNratio_broadcasted, 60, axis=0)  # Repeat along time dimension
 PNratio_data.close()
 
-
 # Path for all the manure N input .nc file
-folder_path = 'C:/Users/zhou111/OneDrive - Wageningen University & Research/2_Data/NP_Input/Processed_data/P_Manure_Input'
-nc_files = glob.glob(os.path.join(folder_path, '*.nc') )                          
+folder_path = 'C:/Users/zhou111/OneDrive - Wageningen University & Research/2_Data/NP_Input/Processed_data/N_Manure_Input'
+nc_files = glob.glob(os.path.join(folder_path, '*.nc'))                          
                      
 for nc_file_name in nc_files:
     
@@ -41,7 +41,7 @@ for nc_file_name in nc_files:
          
          # Add N manure
          new_var1 = nc_file.createVariable(var_name1, 'f4', ('time', 'lon', 'lat'))
-         new_var1[:] = N_manure  # Use slicing to assign the data
+         new_var1[:] = N_manure 
          new_var1.setncattr('description', 'Annual total manure nitrogen input')
          new_var1.setncattr('units', 'kg/ha harvest area')         
          
@@ -49,9 +49,6 @@ for nc_file_name in nc_files:
          new_var2 = nc_file.createVariable(var_name2, 'f4', ('time', 'lon', 'lat'))
          new_var2[:] = P_manure  # Use slicing to assign the data    
          new_var2.setncattr('description', 'Annual total manure phosphorus input')
-         new_var2.setncattr('units', 'kg/ha harvest area')   
-         
+         new_var2.setncattr('units', 'kg/ha harvest area')      
 
     print(f"P manure of {nc_file_name} has been calculated and added")           
-         
-         

Format_Trans_h52nc.py
@@ -3,14 +3,11 @@
 import h5py
 import xarray as xr
 import numpy as np
-import netCDF4
-from netCDF4 import Dataset
 import os
 import glob
 
 folder_path = 'C:/Users/zhou111/OneDrive - Wageningen University & Research/2_Data/NP_Input/Fertilization'
-output_path = 'C:/Users/zhou111/OneDrive - Wageningen University & Research/2_Data/NP_Input/Fertilization/Processed_data'
-#output_path = 'C:/Users/zhou111/OneDrive - Wageningen University & Research/2_Data/NP_Input/Fertilization/Processed_data'
+output_path = 'C:/Users/zhou111/OneDrive - Wageningen University & Research/2_Data/NP_Input/Fertilization/Processed_data/N_Manure_Input_Original'
 h5_files = glob.glob(os.path.join(folder_path, '*.h5'))
 
 for h5_file_name in h5_files:
@@ -23,7 +20,7 @@ for h5_file_name in h5_files:
     
     # Coordinates
     coords = {
-        'year': np.arange(1960, 2020),  # Years 1960-2019
+        'year': np.arange(1961, 2021),  # Years 1961-2020
         'lon': np.linspace(-180, 180, 4320),  # Longitude
         'lat': np.linspace(90, -90, 2160),   # Latitude
     }
@@ -52,5 +49,5 @@ for h5_file_name in h5_files:
         
     print(f"Renamed {h5_file_name} to {nc_file_name}")
     
-        # Close the .h5 file
+    # Close the .h5 file
     h5file.close()